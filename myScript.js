403697aac135ccb299cdcab6baec7af4c8b31322
xiaoyuzhou <595521697@qq.com>
2014-07-30 22:06:20 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	alert("this is good!");
+	var num = 0;
+	$("#tt").on("tap",function(){
+		num++;
+		$("#num").html(num);
+		$("#tt").attr({ src: "image/sad.png" });
+		$("#tt").attr({ src: "image/smile.png" });
+	})
 });
