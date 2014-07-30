123923adf523a741e1e0dc6ad6e0faeb6c063801
xiaoyuzhou <595521697@qq.com>
2014-07-30 22:37:27 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 	$("#tt").on("tap",function(){
 		num++;
 		$("#num").html(num);
-		$("#tt").attr({ src: "image/sad.png" });
-		$("#tt").attr({ src: "image/smile.png" });
+		$("#tt").attr({ src: "http://v2.freep.cn/110_3tb_140730223440foqv512293.png" });
 	})
 });
