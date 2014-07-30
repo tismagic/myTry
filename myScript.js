594b4dbfc075ddb5675322e176b1a2fe6a062bce
xiaoyuzhou <595521697@qq.com>
2014-07-31 05:41:10 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
 	$("#tt").on("vmousedown",function(){
 		num++;
 		$("#num").html(num);
-		$("#tt").attr({ src: "image/sad.png" });
+		$("#tt").attr({ src: "http://v2.freep.cn/110_3tb_140730223440foqv512293.png" });
 	})
 	
 	$("#tt").on("vmouseup",function(){
 		num++;
 		$("#num").html(num);
-		$("#tt").attr({ src: "image/smile.png" });
+		$("#tt").attr({ src: "http://v2.freep.cn/110_3tb_140730223542fta1512293.png" });
 	})
 });
